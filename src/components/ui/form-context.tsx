2e5f8c03c362c7297d1588376c70dbf39b3824d0
Jenqyang <jenqyang@qq.com>
2025-05-17 14:41:08 +0800

    feat: add UI components for form, sidebar, navigation and toast functionality

---
+++ src/components/ui/form-context.tsx
@@ -0,0 +1,52 @@
+import * as React from "react"
+import {
+  FieldPath,
+  FieldValues,
+} from "react-hook-form"
+
+// 将上下文和工具函数移至单独的文件
+export type FormFieldContextValue<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = {
+  name: TName
+}
+
+export const FormFieldContext = React.createContext<FormFieldContextValue>(
+  {} as FormFieldContextValue
+)
+
+export type FormItemContextValue = {
+  id: string
+}
+
+export const FormItemContext = React.createContext<FormItemContextValue>(
+  {} as FormItemContextValue
+)
+
+export const useFormField = () => {
+  const fieldContext = React.useContext(FormFieldContext)
+  const itemContext = React.useContext(FormItemContext)
+  const { getFieldState, formState } = useFormContext()
+
+  const fieldState = getFieldState(fieldContext.name, formState)
+
+  if (!fieldContext) {
+    throw new Error("useFormField should be used within <FormField>")
+  }
+
+  const { id } = itemContext
+
+  return {
+    id,
+    name: fieldContext.name,
+    formItemId: `${id}-form-item`,
+    formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
+    ...fieldState,
+  }
+}
+
+// 重新导出react-hook-form的useFormContext，这样在form.tsx中就不需要再导入了
+import { useFormContext } from "react-hook-form"
+export { useFormContext }
---
+++ src/components/ui/toast-utils.tsx
@@ -0,0 +1,4 @@
+// 将 toast 函数移到单独的文件中
+import { toast } from "sonner"
+
+export { toast }
